d946784cc626463e76f324f84cff74d23b08bb60
wnembzol@gmail.com <98433999+nembdev@users.noreply.github.com>
2022-08-29 23:12:57 -0400

    added Ubuntu Instance

--- main.tf
+++ main.tf
@@ -113,5 +113,30 @@ resource "aws_eip" "main_elastic" {
   vpc = true
   network_interface = aws_network_interface.main_nic.id
   associate_with_private_ip = "10.0.1.50"
+  depends_on = [aws_internet_gateway.main_igw]
 }
 
+resource "aws_instance" "ubuntu_server" {
+  ami = "ami-052efd3df9dad4825"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+
+  key_name = "main_key"
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.main_nic.id
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              sudo apt install apache2 -y
+              sudo systemctl start apache2
+              sudo bash -c "echo your very first web server > /var/www/html/index.html"
+              EOF
+
+  tags = {
+    Name = "Web Server"
+  }
+}
